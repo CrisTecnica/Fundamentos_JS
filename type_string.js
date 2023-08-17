9f591e5ec208b51eba276477e365ac01c54733b3
CrisTecnica <cristianotecnica@gmail.com>
2023-08-17 18:22:20 -0300

    exemplos aula 1

---
+++ type_string.js
@@ -0,0 +1,13 @@
+const texto1 = "Olá, mundo";
+const texto2 = "Olá, mundo!";
+const senha = "senhaSuperSegura1234!";
+const StringDeNumeros = "1234!";
+
+const citacao = "Meu nome é";
+const meuNome = "cristiano";
+
+//concatenação (+)
+
+console.log(citacao +meuNome);
+
+//template string OU template literal
